2276c85f9ee483e5de810dd335bb316a9e513c56
yukinessa <kun.amrin@gmail.com>
2020-12-15 19:29:59 +0700

    feat(controller): create book controller

---
+++ src/controller/book.js
@@ -0,0 +1,44 @@
+const { Book } = require("../db/models");
+
+exports.getAllBook = async (req, res) => {
+  const {
+    page = 0,
+    show = 10,
+    sortBy = "createdAt",
+    orderBy = "ASC"
+  } = req.query;
+  const book = await book.findAndCountAll({
+    order: [[sortBy, orderBy]],
+    offset: page * show,
+    limit: show
+  });
+
+  return res.status(200).json(book);
+};
+
+exports.storeBook = async (req, res) => {
+  const book = await Book.create({ ...req.body });
+  return res.status(200).json(author);
+};
+
+exports.getBookById = async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  return res.status(200).json(book);
+};
+
+exports.updateBookById = async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  const result = await book.update({
+    ...req.body
+  });
+
+  return res.status(200).json(result);
+};
+
+exports.deleteAuthorById = async (req, res) => {
+  const book = await Book.destroy({
+    where: { id: req.params.id }
+  });
+
+  return res.statusCode(200);
+};
