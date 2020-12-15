bb8921b30de2f1a5eff8a75e7078c1b362b918ea
yukinessa <kun.amrin@gmail.com>
2020-12-15 19:52:04 +0700

    feat(router) create router book and import book router on index.js router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const MainRouter = new Router();
 
-const UserRouter = require('./user');
-const AuthorRouter = require('./author');
+const UserRouter = require("./user");
+const AuthorRouter = require("./author");
+const BookRouter = require("./book");
 
-MainRouter.use('/user', UserRouter);
-MainRouter.use('/author', AuthorRouter);
+MainRouter.use("/user", UserRouter);
+MainRouter.use("/author", AuthorRouter);
+MainRouter.use("/book", BookRouter);
 
 module.exports = MainRouter;
