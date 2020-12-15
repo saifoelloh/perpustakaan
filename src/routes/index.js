3ce753418e262ea9e091489bd5fbf263fd03e1db
Grafiters <bayugrafit@gmail.com>
2020-12-15 19:54:43 +0700

    feat(index): add rent route into index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const MainRouter = new Router();
 
 const UserRouter = require('./user');
 const AuthorRouter = require('./author');
+const RentRouter = require('./rent');
 
 MainRouter.use('/user', UserRouter);
 MainRouter.use('/author', AuthorRouter);
+MainRouter.use('/rent', RentRouter);
 
 module.exports = MainRouter;
